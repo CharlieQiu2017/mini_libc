d1e5a031c6337323c67005c14bd1c058726308f7
Charlie Qiu <longfeiqiu2012@gmail.com>
2026-03-30 16:10:13 -0400

    Refactor SPHINCS+ sign to have separate randomize/non-randomize versions

--- include/crypto/sign/sphincs/sphincs.h
+++ include/crypto/sign/sphincs/sphincs.h
@@ -6,7 +6,9 @@
 
 void sphincs_128f_shake_simple_gen_key (unsigned char * sk_out, unsigned char * pk_out);
 
-void sphincs_128f_shake_simple_sign (const unsigned char * sk, const unsigned char * msg, size_t msg_len, _Bool randomize, unsigned char * out);
+void sphincs_128f_shake_simple_sign (const unsigned char * sk, const unsigned char * msg, size_t msg_len, unsigned char * out);
+
+void sphincs_128f_shake_simple_sign_randomize (const unsigned char * sk, const unsigned char * msg, size_t msg_len, unsigned char * out);
 
 uint64_t sphincs_128f_shake_simple_verify (const unsigned char * pk, const unsigned char * msg, size_t msg_len, const unsigned char * sig);
 
