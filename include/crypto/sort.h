ca441ee63a2c9356580c7e03661e1627fe466924
Charlie Qiu <longfeiqiu2012@gmail.com>
2026-09-13 19:58:04 -0400

    Add comment on safe_sort

--- include/crypto/sort.h
+++ include/crypto/sort.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+/* Callers of these functions must ensure 1 < len <= (1ull << 63) */
 void safe_sort_uint32 (uint32_t * input, size_t len);
 void safe_sort_uint64 (uint64_t * input, size_t len);
 
